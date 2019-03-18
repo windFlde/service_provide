27f0560bc58376297bd036a62c6c9bd31308fbf3
XZH <1170723706@qq.com>
2019-03-18 14:56:06 +0800

    开通会员

--- service_provide/src/main/java/com/jk/mapper/PlayMapper.java
+++ service_provide/src/main/java/com/jk/mapper/PlayMapper.java
@@ -1,8 +1,11 @@
 package com.jk.mapper;
 
+import com.jk.bean.User;
 import org.apache.ibatis.annotations.Param;
 
 public interface PlayMapper {
 
     void updateBlance(@Param("jg") Integer jg, @Param("id") Integer id);
+
+    void addVIP(User user);
 }
--- service_provide/src/main/java/com/jk/service/PlayService.java
+++ service_provide/src/main/java/com/jk/service/PlayService.java
@@ -1,6 +1,10 @@
 package com.jk.service;
 
+import com.jk.bean.User;
+
 public interface PlayService {
 
     void updateBlance(Integer jg,Integer id);
+
+    void addVIP(User user);
 }
