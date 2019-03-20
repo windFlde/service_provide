84d0cdfadb9c647ac8b03649b893d3c4200ac2e9
XZH <1170723706@qq.com>
2019-03-20 16:18:10 +0800

    vip

---
+++ service_provide/src/main/java/com/jk/bean/VipInfo.java
@@ -0,0 +1,13 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+@Data
+public class VipInfo {
+    Integer id;
+    String vipone;
+    String vipmoney;
+    String vipthree;
+    String vipfour;
+    String info;
+}
